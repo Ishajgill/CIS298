3066391091d14a3041ea09f250b9c3ed20badc55
ijgill <156219168+Ishajgill@users.noreply.github.com>
2025-01-10 17:27:02 -0500

    practiceQuestion

---
+++ PythonProjectPractice/main.py
@@ -0,0 +1,15 @@
+import math
+college_credits_needed = int(input("How many college credits you need?"))
+credits_completed = int(input("How many credits you have completed?"))
+credits_taking_per_semester = int(input("Credits taken per semester"))
+credits_remaining = int(college_credits_needed - credits_completed)
+semesters_needed = (math.ceil(credits_remaining/credits_taking_per_semester))
+RATE = 600
+def tuition_to_be_paid(college_credits_needed, credits_completed):
+    if credits_taking_per_semester < 12:
+        print("Tuition is ",RATE * credits_remaining  )
+    else:
+        print("Tuition is ",math.floor(credits_remaining/credits_taking_per_semester)*7200 + (credits_remaining % credits_taking_per_semester*RATE))
+
+print(tuition_to_be_paid(120,60))
+
